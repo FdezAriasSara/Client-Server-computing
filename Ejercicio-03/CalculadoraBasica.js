d74cd8d50d9da49d7c3cdebeb0bbd27c3871315c
Sara Fernández Arias <79209608+FdezAriasSara@users.noreply.github.com>
2022-11-18 15:58:19 +0100

    Enhancements of my calculators
    - Extracted responsability of storing the result in memory and operand1 assigment into an independent method. ( code reutilization)
    - Extraction responsability of neutral element in simple computations.
    -Extraction of constants ERROR and VACIO

--- Ejercicio-03/CalculadoraBasica.js
+++ Ejercicio-03/CalculadoraBasica.js
@@ -1,13 +1,16 @@
 "use strict";
+const ERROR="ERROR";
 const PUNTO = '.'
+const VACIO=" "
 class CalculadoraBasica {
 
     constructor() {
-        this.pantalla = ' ';
-        this.memoria = 0;
+        this.pantalla = VACIO;
+        this.memoria = Number(0);
         this.operando1 = null;
         this.operando2 = null;
         this.operacion = "";
+        this.resultado=Number(0);
 
     }
     actualizarPantalla() {
@@ -32,8 +35,6 @@ class CalculadoraBasica {
     /*En caso de que tras introducir una punto se presione el simbolo de una operación, se borrará automáticamente*/
     quitarSimboloDecimales(simbolo) {
         if (this.pantalla.length > 0) {
-            //TODOOOOOOOOOOOOOOOOOOOOOO
-            console.log(this.pantalla.charAt(-1))
             if (this.pantalla.charAt(-1)=== simbolo) {
                 this.borrarUltimoDigito();
             }
@@ -44,28 +45,28 @@ class CalculadoraBasica {
     suma() {
         this.quitarSimboloDecimales(PUNTO);
         this.asignarOperandos();
-        this.ejecutarCalculos();
+        this.ejecutarOperaciónBasica();
         this.operacion = '+';
 
     }
     resta() {
         this.quitarSimboloDecimales(PUNTO);
         this.asignarOperandos();
-        this.ejecutarCalculos();
+        this.ejecutarOperaciónBasica();
         this.operacion = '-';
 
     }
     multiplicacion() {
         this.quitarSimboloDecimales(PUNTO);       
         this.asignarOperandos();     
-        this.ejecutarCalculos();
+        this.ejecutarOperaciónBasica();
         this.operacion = '*';
 
     }
     division() {
         this.quitarSimboloDecimales(PUNTO);
         this.asignarOperandos();
-        this.ejecutarCalculos();
+        this.ejecutarOperaciónBasica();
         this.operacion = '/';
 
     }
@@ -81,8 +82,8 @@ class CalculadoraBasica {
         this.operando1 = Number(this.pantalla)
         this.operando2 = Number(this.memoria)
         this.operacion = "-";
-        this.ejecutarCalculos();
-        this.pantalla = " ";
+        this.ejecutarOperaciónBasica();
+        this.pantalla =VACIO;
 
     }
     sumarMemoria() {
@@ -90,20 +91,25 @@ class CalculadoraBasica {
         this.operando1 = Number(this.pantalla)
         this.operando2 = Number(this.memoria)
         this.operacion = "+";
-        this.ejecutarCalculos();
-        this.pantalla = " ";
+        this.ejecutarOperaciónBasica();
+        this.pantalla =VACIO;
 
     }
 
     borrar() {
 
-        this.pantalla = ' ';
+        this.pantalla = VACIO;
         this.actualizarPantalla();
     }
     borrarError() {
-        //TODO -> BOTON CE
-
-    }
+        if(this.pantalla===ERROR){
+         this.pantalla= VACIO;
+ 
+        }else{
+         this.borrarUltimoDigito();
+        }
+ 
+     }
     borrarUltimoDigito() {
         this.pantalla = this.pantalla.slice(0, -1)
         this.actualizarPantalla();
@@ -112,7 +118,7 @@ class CalculadoraBasica {
         this.quitarSimboloDecimales(PUNTO);
         this.actualizarPantalla();
         this.asignarOperandos();
-        this.ejecutarCalculos();
+        this.ejecutarOperaciónBasica();
         this.operando1 = null;
         this.operando2 = null;
     }
@@ -121,19 +127,20 @@ class CalculadoraBasica {
         this.asignarOperandoUnarias();
         this.operando2 = Number(this.pantalla);
         this.operacion = "%"
-        this.pantalla = this.operando1 * Number(eval(this.operando2 + "/" + Number(100)))
+        this.resultado = this.operando1 * Number(eval(this.operando2 + "/" + Number(100)))
+        this.procesarResultado();
         this.actualizarPantalla();
-        this.pantalla = " ";
+        this.pantalla =  VACIO;
 
     }
     raizCuadrada() {
         this.quitarSimboloDecimales(PUNTO);
         this.asignarOperandoUnarias();
         this.operacion = 'sqrt'
-        this.memoria = Math.sqrt(this.operando1);//devuelve un number
-        this.pantalla = this.memoria;
+        this.resultado=  Math.sqrt(this.operando1);//devuelve un number
+        this.procesarResultado();
         this.actualizarPantalla();
-        this.pantalla = " ";
+        this.pantalla =  VACIO;
 
     }
 
@@ -142,11 +149,11 @@ class CalculadoraBasica {
         this.asignarOperandoUnarias();
         this.operando2 = Number(-1);
         this.operacion = "*";
-        this.ejecutarCalculos();
+        this.ejecutarOperaciónBasica();
     }
     asignarOperandoUnarias() {
        //Si la pantalla está vacía no asigno operandos, ya que Number( ) devuelve 0, produciendo resultados erróneos en operaciones como la multiplicación                                                             
-        if (this.pantalla != " ") {
+        if (this.pantalla !=  VACIO) {
             this.operando1 = Number(this.pantalla);
         } else {
             this.operando1 = this.memoria;
@@ -154,7 +161,7 @@ class CalculadoraBasica {
     }
     asignarOperandos() {
         
-        if(this.pantalla!=" " ){
+        if(this.pantalla!= VACIO){
             //Si la pantalla está vacía no asigno operandos, ya que Number( ) devuelve 0, produciendo resultados erróneos en operaciones como la multiplicación 
             if ( this.operando1 !== null && this.operando2 === null) {
             
@@ -166,7 +173,16 @@ class CalculadoraBasica {
         }
        
     }
-    ejecutarCalculos() {
+    /**
+     * Método que ejecuta los calculos de las operaciones básicas: Suma, resta , multiplicación y división.
+    * Estas presentan una estructura similar:
+    *   -En caso de que el segundo operando sea null , se le asigna un elemento neutro.
+    *   -Una vez estemos seguros de que ningún operando es null, se realiza la operación llamando a eval en #operaciónSimple, y se guarda
+    *   el resultado de la misma en memoria y en el operando 1. En caso de ser errónea se muestra el mensaje "error"
+    *   -Una vez hemos hecho el cálculo, establecemos como null el segundo operando.ç
+    *   -Actualizamos la pantalla y nos preparamos para las siguientes operaciones. (vaciando el elemento pantalla internamente)
+    */
+    ejecutarOperaciónBasica() {
 
         switch (this.operacion) {
             case "*":
@@ -192,7 +208,7 @@ class CalculadoraBasica {
 
         }
         this.actualizarPantalla();
-        this.pantalla = " ";
+        this.pantalla =  VACIO;;
 
     }
     /**
@@ -208,20 +224,37 @@ class CalculadoraBasica {
         }
     }
 
+    /**
+     * Este método hace uso de "Eval" para obtener el resultado de las operaciones simples( x , / , + , -)
+     * Se realiza mediante este manejo de las posibles excepciones producidas durante el proceso de evaluación de la 
+     * expresión que representa la operación.
+     */
     #operacionSimple() {
 
         try {
-
-            this.pantalla = eval(this.operando1 + this.operacion + this.operando2);
-            this.memoria = Number(this.pantalla);
-            this.operando1 = this.memoria;
-           
+            this.resultado=eval(this.operando1 + this.operacion + this.operando2);
+            this.procesarResultado();
         }
         catch (err) {
+            //En caso de que eval de lugar a una excepción, se mostrará el mensaje de error.
             console.log(err)
-            this.pantalla = "ERROR"
+            this.pantalla =ERROR;
         }
 
+    }
+    /*
+     * Este método recibe el resultado de una operación y lo guarda en memoria y en el operando1. 
+     * En caso de que la expresión evalue a NaN , memoria y operando 1 no serán modificados, 
+     * y el mensaje "ERROR" se mostrará en la pantalla.   
+     */
+    procesarResultado() {
+        if(!isNaN(this.resultado)){
+            this.memoria =this.resultado;//devuelve un number
+            this.pantalla = this.memoria;
+        }else{
+            this.pantalla=ERROR;
+        }
+        
     }
 
 }
